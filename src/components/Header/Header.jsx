d0929fc29453a1dc5de614498da11ba66c524bd7
Geoffrey <78374883+Geo-M-Geo@users.noreply.github.com>
2024-08-21 21:11:07 +0200

    header menu html

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,49 @@
 import { Link } from 'react-router-dom'
 import '../Header/Header.scss'
-import Menu from '../Menu/Menu'
 import feather from '../../assets/plume3.png'
 import { useState } from 'react'
-import dropdown from "../../assets/icon-menu.png";
+import down from "../../assets/chevron-down.png";
+import up from "../../assets/chevron-up.png";
+
+
 
 function Header() {
+const [open, setOpen] = useState(false);
+
+const handleOpen = () => {
+    setOpen(!open);
+};
+
     return (
-        <div className='header'>
-                <Link className='header__logo-title' to="/">
-                    <img className='header__logo-title__logo' alt='Feather logo of the website' src={feather}/>
-                    <h1 className='header__logo-title__title'  >Les Mésanges</h1>
-                </Link>
-            <div className='header__menu'>
-                    <img className='header__menu__dropdown-logo' src={dropdown}></img>
-                <div className='header__menu__navbar'>
-                    <Link className='header__menu__navbar__link' to="/">Accueil</Link>
-                    <Link className='header__menu__navbar__link' to="/equipements">Equipements</Link>
-                </div>
-            </div>
+    <div className='header'>
+        <Link className='header__logo-title' to="/">
+            <img className='header__logo-title__logo' alt='Feather logo of the website' src={feather}/>
+            <h1 className='header__logo-title__title'  >Les Mésanges</h1>
+        </Link>
+        <div className='header__menu__normal'>
+                <Link className='header__menu__normal__link' to="/">Accueil</Link>
+                <Link className='header__menu__normal__link' to="/equipements">Equipements</Link>
+                <Link className='header__menu__normal__link' to="/loisirs">Loisirs</Link>
         </div>
-    )
-}
-export default Header
+        <div className="header__menu__small-screen">
+            <button onClick={handleOpen} className='header__menu__small-screen__button'>
+            {open ? (
+                <div className='header__menu__small-screen__opening' >
+                    <img className='header__menu__small-screen__opening__logo' src={up} alt='arrow up'/>
+                    <div className='header__menu__small-screen__opening__links'>
+                        <Link className='header__menu__small-screen__opening__links__link' to="/">Accueil</Link>
+                        <Link className='header__menu__small-screen__opening__links__link' to="/equipements">Equipements</Link>
+                        <Link className='header__menu__small-screen__opening__links__link' to="/loisirs">Loisirs</Link>
+                    </div>
+                </div>
+            ) : <img className='header__menu__small-screen__button__logo' src={down} alt='arrow up'/> }
+            </button>
+        </div>
+    </div>
+    );
+};
+
+    export default Header;
+
+
+// https://stackoverflow.com/questions/62835987/react-rotate-chevron-on-click
